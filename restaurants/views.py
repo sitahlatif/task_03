9d6f55a1e385196104bae1384549216029742ba8
sitahlatif <sitah27@hotmail.com>
2019-02-10 16:39:50 +0300

    sita

--- restaurants/views.py
+++ restaurants/views.py
@@ -5,7 +5,20 @@ def welcome(request):
 
 def restaurant_list(request):
 
-    context = {
+    context = { 'my_list':[
+    {
+    'resturant_name': 'Latavolla',
+    'food_type':'Italian Food'
+    },
+    {
+    'resturant_name': 'Simple But Dynamic',
+    'food_type' :'Amrican Food'
+    },
+    {
+    'resturant_name': 'Shormer',
+    'food_type': 'Arbian Food'
+    }
+    ],
 
     }
     return render(request, 'list.html', context)
@@ -13,7 +26,10 @@ def restaurant_list(request):
 
 def restaurant_detail(request):
 
-    context = {
+    context = { 'my_object':{
+    'resturant_name': 'Herfi',
+    'food_type': 'Fast food'
+    },
 
     }
     return render(request, 'detail.html', context)
